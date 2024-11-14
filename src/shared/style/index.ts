7db19aae9430db16475b05cc996296d0b230a404
lsjoon <lsjoon.dev@gmail.com>
2024-11-15 07:11:14 +0900

    style: modify global layout

--- src/shared/style/index.ts
+++ src/shared/style/index.ts
@@ -6,16 +6,26 @@ export const RootLayout = styled.div`
 
   width: 100vw;
   height: 100vh;
+  padding: 5rem;
+  box-sizing: border-box;
 `;
 
-export const MainLayout = styled.div``;
+export const MainLayout = styled.div`
+  flex: 1;
+  display: flex;
+
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+`;
 
 export const Wrapper = styled.div`
+  flex: 1;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
 
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
 `;
